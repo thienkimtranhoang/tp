edd6f14e8a5bd15b76c5bb47c5e8806c15f19123
dariusyawningwhiz <dariusleeql@gmail.com>
2025-03-20 11:28:54 +0800

    daius junit and assertion

--- src/main/java/budgetflow/MainTracker.java
+++ src/main/java/budgetflow/MainTracker.java
@@ -3,6 +3,7 @@ package budgetflow;
 public class MainTracker {
     public static void main(String[] args) {
         FinanceTracker financeTracker = new FinanceTracker();
+        assert false : "dummy assertion set to fail";
         financeTracker.run();
     }
 }
