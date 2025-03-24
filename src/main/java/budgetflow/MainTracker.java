bfdfb6a7a99dd7d96f027b758b2527f29fadbe24
dariusyawningwhiz <dariusleeql@gmail.com>
2025-03-24 23:50:17 +0800

    aaa

--- src/main/java/budgetflow/MainTracker.java
+++ src/main/java/budgetflow/MainTracker.java
@@ -1,7 +1,13 @@
 package budgetflow;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MainTracker {
     public static void main(String[] args) {
+        Logger rootLogger = Logger.getLogger("");
+        rootLogger.setLevel(Level.WARNING);
+        Logger parserLogger = Logger.getLogger("budgetflow.parser.Parser");
+        parserLogger.setLevel(Level.WARNING);
         FinanceTracker financeTracker = new FinanceTracker();
         financeTracker.run();
     }
