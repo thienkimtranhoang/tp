b4e7c8b6a857139b8afe86f045fe19265fb52259
QuyDatNguyen <142130714+QuyDatNguyen@users.noreply.github.com>
2025-03-28 19:58:56 +0800

    checkstyle fix again

--- src/main/java/budgetflow/exception/InvalidDateException.java
+++ src/main/java/budgetflow/exception/InvalidDateException.java
@@ -2,6 +2,6 @@ package budgetflow.exception;
 
 public class InvalidDateException extends FinanceException {
     public InvalidDateException(String message) {
-      super(message);
+        super(message);
     }
 }
--- src/main/java/budgetflow/exception/InvalidKeywordException.java
+++ src/main/java/budgetflow/exception/InvalidKeywordException.java
@@ -2,6 +2,6 @@ package budgetflow.exception;
 
 public class InvalidKeywordException extends FinanceException {
     public InvalidKeywordException(String message) {
-      super(message);
+        super(message);
     }
 }
