b24b7402f49357a9ea293738fe17cad24c487b44
IgoyAI <aagdeyogipramana@mail.ugm.ac.id>
2025-04-05 19:23:20 +0800

    Use switch case for better code readability #172

--- src/main/java/budgetflow/parser/Parser.java
+++ src/main/java/budgetflow/parser/Parser.java
@@ -63,44 +63,114 @@ public class Parser {
      */
     public static Command getCommandFromInput(String input) throws UnknownCommandException {
         logger.info("Processing command: " + input);
-        if (input.startsWith(COMMAND_ADD_INCOME)) {
-            return new AddIncomeCommand(input);
-        } else if (input.startsWith(COMMAND_SET_SAVING_GOAL)) {
-            return new SetSavingGoalCommand(input);
-        } else if (input.startsWith(COMMAND_LOG_EXPENSE)) {
-            return new LogExpenseCommand(input);
-        } else if (input.startsWith(COMMAND_DELETE_INCOME)) {
-            return new DeleteIncomeCommand(input);
-        } else if (COMMAND_LIST_INCOME.equals(input)) {
-            return new ListIncomeCommand();
-        } else if (input.startsWith(COMMAND_DELETE_EXPENSE)) {
-            return new DeleteExpenseCommand(input);
-        } else if (input.equals(COMMAND_VIEW_ALL_EXPENSES)) {
-            return new ViewAllExpensesCommand();
-        } else if (input.startsWith(COMMAND_FIND_EXPENSE)) {
-            return new FindExpenseCommand(input);
-        } else if (input.equals(COMMAND_EXIT)) {
-            return new ExitCommand();
-        } else if (input.startsWith(COMMAND_COMPARE)) {
-            return new CompareExpenseCommand(input);
-        } else if (input.startsWith(COMMAND_UPDATE_EXPENSE)) {
-            return new UpdateExpenseCommand(input);
-        } else if (input.startsWith(COMMAND_UPDATE_INCOME)) {
-            return new UpdateIncomeCommand(input);
-        } else if (input.equals(COMMAND_FILTER_INCOME)) {
-            // When the user types exactly "filter-income", display the usage guide.
-            return new FilterIncomeCommand(input);
-        } else if (input.startsWith(COMMAND_FILTER_INCOME_DATE)) {
-            return new FilterIncomeByDateCommand(input);
-        } else if (input.startsWith(COMMAND_FILTER_INCOME_AMOUNT)) {
-            return new FilterIncomeByAmountCommand(input);
-        } else if (input.startsWith(COMMAND_FILTER_INCOME_CATEGORY)) {
-            return new FilterIncomeByCategoryCommand(input);
-        } else if (input.startsWith(COMMAND_HELP)) {
-            return new HelpCommand();
-        } else {
-            logger.warning(ERROR_UNKNOWN_COMMAND + input);
-            throw new UnknownCommandException();
+        CommandIdentifier commandId = identifyCommand(input.trim());
+        switch (commandId) {
+            case ADD_INCOME:
+                return new AddIncomeCommand(input);
+            case SET_SAVING_GOAL:
+                return new SetSavingGoalCommand(input);
+            case LOG_EXPENSE:
+                return new LogExpenseCommand(input);
+            case DELETE_INCOME:
+                return new DeleteIncomeCommand(input);
+            case LIST_INCOME:
+                return new ListIncomeCommand();
+            case DELETE_EXPENSE:
+                return new DeleteExpenseCommand(input);
+            case VIEW_ALL_EXPENSES:
+                return new ViewAllExpensesCommand();
+            case FIND_EXPENSE:
+                return new FindExpenseCommand(input);
+            case EXIT:
+                return new ExitCommand();
+            case COMPARE:
+                return new CompareExpenseCommand(input);
+            case UPDATE_EXPENSE:
+                return new UpdateExpenseCommand(input);
+            case UPDATE_INCOME:
+                return new UpdateIncomeCommand(input);
+            case FILTER_INCOME:
+                return new FilterIncomeCommand(input);
+            case FILTER_INCOME_DATE:
+                return new FilterIncomeByDateCommand(input);
+            case FILTER_INCOME_AMOUNT:
+                return new FilterIncomeByAmountCommand(input);
+            case FILTER_INCOME_CATEGORY:
+                return new FilterIncomeByCategoryCommand(input);
+            case HELP:
+                return new HelpCommand();
+            default:
+                logger.warning(ERROR_UNKNOWN_COMMAND + input);
+                throw new UnknownCommandException();
         }
+    }
+
+    /**
+     * Identifies the command type based on the input string.
+     *
+     * @param input the trimmed user input.
+     * @return the corresponding CommandIdentifier.
+     */
+    private static CommandIdentifier identifyCommand(String input) {
+        if (input.startsWith(COMMAND_ADD_INCOME)) {
+            return CommandIdentifier.ADD_INCOME;
+        } else if (input.startsWith(COMMAND_SET_SAVING_GOAL)) {
+            return CommandIdentifier.SET_SAVING_GOAL;
+        } else if (input.startsWith(COMMAND_LOG_EXPENSE)) {
+            return CommandIdentifier.LOG_EXPENSE;
+        } else if (input.startsWith(COMMAND_DELETE_INCOME)) {
+            return CommandIdentifier.DELETE_INCOME;
+        } else if (COMMAND_LIST_INCOME.equals(input)) {
+            return CommandIdentifier.LIST_INCOME;
+        } else if (input.startsWith(COMMAND_DELETE_EXPENSE)) {
+            return CommandIdentifier.DELETE_EXPENSE;
+        } else if (COMMAND_VIEW_ALL_EXPENSES.equals(input)) {
+            return CommandIdentifier.VIEW_ALL_EXPENSES;
+        } else if (input.startsWith(COMMAND_FIND_EXPENSE)) {
+            return CommandIdentifier.FIND_EXPENSE;
+        } else if (COMMAND_EXIT.equals(input)) {
+            return CommandIdentifier.EXIT;
+        } else if (input.startsWith(COMMAND_COMPARE)) {
+            return CommandIdentifier.COMPARE;
+        } else if (input.startsWith(COMMAND_UPDATE_EXPENSE)) {
+            return CommandIdentifier.UPDATE_EXPENSE;
+        } else if (input.startsWith(COMMAND_UPDATE_INCOME)) {
+            return CommandIdentifier.UPDATE_INCOME;
+        } else if (COMMAND_FILTER_INCOME.equals(input)) {
+            return CommandIdentifier.FILTER_INCOME;
+        } else if (input.startsWith(COMMAND_FILTER_INCOME_DATE)) {
+            return CommandIdentifier.FILTER_INCOME_DATE;
+        } else if (input.startsWith(COMMAND_FILTER_INCOME_AMOUNT)) {
+            return CommandIdentifier.FILTER_INCOME_AMOUNT;
+        } else if (input.startsWith(COMMAND_FILTER_INCOME_CATEGORY)) {
+            return CommandIdentifier.FILTER_INCOME_CATEGORY;
+        } else if (input.startsWith(COMMAND_HELP)) {
+            return CommandIdentifier.HELP;
+        }
+        return CommandIdentifier.UNKNOWN;
+    }
+
+    /**
+     * Enum to classify command types based on user input.
+     */
+    private enum CommandIdentifier {
+        ADD_INCOME,
+        SET_SAVING_GOAL,
+        LOG_EXPENSE,
+        DELETE_INCOME,
+        LIST_INCOME,
+        DELETE_EXPENSE,
+        VIEW_ALL_EXPENSES,
+        FIND_EXPENSE,
+        EXIT,
+        COMPARE,
+        UPDATE_EXPENSE,
+        UPDATE_INCOME,
+        FILTER_INCOME,
+        FILTER_INCOME_DATE,
+        FILTER_INCOME_AMOUNT,
+        FILTER_INCOME_CATEGORY,
+        HELP,
+        UNKNOWN
     }
 }
