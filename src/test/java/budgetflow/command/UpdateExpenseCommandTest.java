30cfb5702bc54f2b1b24df7e26d52c01056581e7
dariusyawningwhiz <dariusleeql@gmail.com>
2025-04-06 21:02:16 +0800

    update test case for update-expense

--- src/test/java/budgetflow/command/UpdateExpenseCommandTest.java
+++ src/test/java/budgetflow/command/UpdateExpenseCommandTest.java
@@ -2,6 +2,8 @@ package budgetflow.command;
 
 import budgetflow.exception.InvalidDateException;
 import budgetflow.exception.InvalidNumberFormatException;
+import budgetflow.exception.MissingCategoryException;
+import budgetflow.exception.MissingDescriptionException;
 import budgetflow.expense.Expense;
 import budgetflow.expense.ExpenseList;
 import budgetflow.income.Income;
@@ -28,166 +30,98 @@ public class UpdateExpenseCommandTest {
     }
 
     @Test
-    void updateExpense_success() throws Exception {
-        UpdateExpenseCommand command = new UpdateExpenseCommand(
-                "update-expense 1 category/Dining amt/12.00 desc/Dinner d/03-01-2024");
+    void updateExpense_invalidCategoryFormat() {
+        UpdateExpenseCommand command = new UpdateExpenseCommand("update-expense 1 category/Food#123");
+        Exception exception = assertThrows(MissingCategoryException.class, () -> command.execute(incomes, expenseList));
+        assertEquals("Error: Invalid category. It must contain only alphanumeric characters.", exception.getMessage());
+    }
+
+    @Test
+    void updateExpense_invalidDescriptionFormat() {
+        UpdateExpenseCommand command = new UpdateExpenseCommand("update-expense 1 desc/Lunch!!!");
+        Exception exception = assertThrows(MissingDescriptionException.class, () -> command.execute(incomes, expenseList));
+        assertEquals("Error: Invalid description. It must contain only alphanumeric characters.", exception.getMessage());
+    }
+
+    @Test
+    void updateExpense_amountExceeds7Digits() {
+        UpdateExpenseCommand command = new UpdateExpenseCommand("update-expense 1 amt/10000000.00");
+        Exception exception = assertThrows(InvalidNumberFormatException.class, () -> command.execute(incomes, expenseList));
+        assertEquals("Amount exceeds 7 digits. Please enter a number with up to 7 digits.", exception.getMessage());
+    }
+
+    @Test
+    void updateExpense_amountMoreThan2DecimalPlaces() {
+        UpdateExpenseCommand command = new UpdateExpenseCommand("update-expense 1 amt/10.123");
+        Exception exception = assertThrows(InvalidNumberFormatException.class, () -> command.execute(incomes, expenseList));
+        assertEquals("Amount must have at most 2 decimal places.", exception.getMessage());
+    }
+
+    @Test
+    void updateExpense_noValidUpdates() throws Exception {
+        UpdateExpenseCommand command = new UpdateExpenseCommand("update-expense 1");
+        command.execute(incomes, expenseList);
+        Expense updatedExpense = expenseList.get(0);
+        assertEquals("Food", updatedExpense.getCategory());
+        assertEquals("Lunch", updatedExpense.getDescription());
+        assertEquals(10.50, updatedExpense.getAmount());
+        assertEquals("01-01-2024", updatedExpense.getDate());
+    }
+
+    @Test
+    void updateExpense_validFloatingPointAmount() throws Exception {
+        UpdateExpenseCommand command = new UpdateExpenseCommand("update-expense 1 amt/99.99");
+        command.execute(incomes, expenseList);
+        Expense updatedExpense = expenseList.get(0);
+        assertEquals(99.99, updatedExpense.getAmount());
+    }
+
+    @Test
+    void updateExpense_storageUpdateCalled() throws Exception {
+        UpdateExpenseCommand command = new UpdateExpenseCommand("update-expense 1 category/Entertainment amt/20.00");
         command.execute(incomes, expenseList);
 
         Expense updatedExpense = expenseList.get(0);
-        assertEquals("Dining", updatedExpense.getCategory());
-        assertEquals("Dinner", updatedExpense.getDescription());
-        assertEquals(12.00, updatedExpense.getAmount());
-        assertEquals("03-01-2024", updatedExpense.getDate());
+        assertEquals("Entertainment", updatedExpense.getCategory());
+        assertEquals(20.00, updatedExpense.getAmount());
     }
 
     @Test
     void updateExpense_missingIndex() {
         UpdateExpenseCommand command = new UpdateExpenseCommand("update-expense");
-        Exception exception = assertThrows(InvalidNumberFormatException.class, () ->
-                command.execute(incomes, expenseList));
+        Exception exception = assertThrows(InvalidNumberFormatException.class, () -> command.execute(incomes, expenseList));
         assertEquals("Error: Index is required.", exception.getMessage());
     }
 
     @Test
-    void updateExpense_invalidIndexFormat() {
+    void updateExpense_nonNumericIndex() {
         UpdateExpenseCommand command = new UpdateExpenseCommand("update-expense abc category/Food");
-        Exception exception = assertThrows(InvalidNumberFormatException.class, () ->
-                command.execute(incomes, expenseList));
+        Exception exception = assertThrows(InvalidNumberFormatException.class, () -> command.execute(incomes, expenseList));
         assertEquals("Error: Index must be a number.", exception.getMessage());
     }
 
     @Test
     void updateExpense_indexOutOfBounds() {
         UpdateExpenseCommand command = new UpdateExpenseCommand("update-expense 5 category/Food");
-        Exception exception = assertThrows(InvalidNumberFormatException.class, () ->
-                command.execute(incomes, expenseList));
+        Exception exception = assertThrows(InvalidNumberFormatException.class, () -> command.execute(incomes, expenseList));
         assertEquals("Error: Expense entry not found.", exception.getMessage());
     }
-
     @Test
     void updateExpense_emptyExpenseList() {
-        expenseList = new ExpenseList();
+        ExpenseList emptyExpenseList = new ExpenseList();
         UpdateExpenseCommand command = new UpdateExpenseCommand("update-expense 1 category/Food");
-        Exception exception = assertThrows(InvalidNumberFormatException.class, () ->
-                command.execute(incomes, expenseList));
+        Exception exception = assertThrows(InvalidNumberFormatException.class, () -> command.execute(incomes, emptyExpenseList));
         assertEquals("Error: No expense entries exist to update.", exception.getMessage());
     }
 
     @Test
-    void updateExpense_noChange() throws Exception {
-        UpdateExpenseCommand command = new UpdateExpenseCommand("update-expense 1");
+    void updateExpense_multipleValidUpdates() throws Exception {
+        UpdateExpenseCommand command = new UpdateExpenseCommand("update-expense 1 category/Entertainment desc/Movie amt/15.00 d/05-04-2024");
         command.execute(incomes, expenseList);
-
-        Expense expense = expenseList.get(0);
-        assertEquals("Food", expense.getCategory());
-        assertEquals("Lunch", expense.getDescription());
-        assertEquals(10.50, expense.getAmount());
-        assertEquals("01-01-2024", expense.getDate());
-    }
-
-    @Test
-    void updateExpense_invalidDateFormat() {
-        UpdateExpenseCommand command = new UpdateExpenseCommand(
-                "update-expense 1 category/Food amt/15.00 desc/Dinner d/32-01-2024");
-        Exception exception = assertThrows(InvalidDateException.class, () -> command.execute(incomes, expenseList));
-        assertEquals("Error: Invalid date format. Usage: DD-MM-YYYY", exception.getMessage());
-    }
-
-    @Test
-    void updateExpense_updateOnlyCategory() throws Exception {
-        UpdateExpenseCommand command = new UpdateExpenseCommand("update-expense 1 category/Dining");
-        command.execute(incomes, expenseList);
-
         Expense updatedExpense = expenseList.get(0);
-        assertEquals("Dining", updatedExpense.getCategory());
-        assertEquals("Lunch", updatedExpense.getDescription());
-        assertEquals(10.50, updatedExpense.getAmount());
-        assertEquals("01-01-2024", updatedExpense.getDate());
-    }
-
-    @Test
-    void updateExpense_updateOnlyAmount() throws Exception {
-        UpdateExpenseCommand command = new UpdateExpenseCommand("update-expense 1 amt/20.00");
-        command.execute(incomes, expenseList);
-
-        Expense updatedExpense = expenseList.get(0);
-        assertEquals("Food", updatedExpense.getCategory());
-        assertEquals("Lunch", updatedExpense.getDescription());
-        assertEquals(20.00, updatedExpense.getAmount());
-        assertEquals("01-01-2024", updatedExpense.getDate());
-    }
-
-    @Test
-    void updateExpense_updateOnlyDescription() throws Exception {
-        UpdateExpenseCommand command = new UpdateExpenseCommand("update-expense 1 desc/Dinner");
-        command.execute(incomes, expenseList);
-
-        Expense updatedExpense = expenseList.get(0);
-        assertEquals("Food", updatedExpense.getCategory());
-        assertEquals("Dinner", updatedExpense.getDescription());
-        assertEquals(10.50, updatedExpense.getAmount());
-        assertEquals("01-01-2024", updatedExpense.getDate());
-    }
-
-    @Test
-    void updateExpense_updateOnlyDate() throws Exception {
-        UpdateExpenseCommand command = new UpdateExpenseCommand("update-expense 1 d/02-02-2024");
-        command.execute(incomes, expenseList);
-
-        Expense updatedExpense = expenseList.get(0);
-        assertEquals("Food", updatedExpense.getCategory());
-        assertEquals("Lunch", updatedExpense.getDescription());
-        assertEquals(10.50, updatedExpense.getAmount());
-        assertEquals("02-02-2024", updatedExpense.getDate());
-    }
-
-    @Test
-    void updateExpense_invalidDateFormatOnUpdate() {
-        UpdateExpenseCommand command = new UpdateExpenseCommand(
-                "update-expense 1 category/Food amt/10.00 desc/Lunch d/99-99-2024");
-        Exception exception = assertThrows(InvalidDateException.class, () -> command.execute(incomes, expenseList));
-        assertEquals("Error: Invalid date format. Usage: DD-MM-YYYY", exception.getMessage());
-    }
-
-    @Test
-    void updateExpense_updateStorageCalled() throws Exception {
-        UpdateExpenseCommand command = new UpdateExpenseCommand("update-expense 1 category/Transport amt/15.00");
-        command.execute(incomes, expenseList);
-    }
-
-    @Test
-    void updateExpense_largeIndex() {
-        UpdateExpenseCommand command = new UpdateExpenseCommand("update-expense 999999 category/Food");
-        Exception exception = assertThrows(InvalidNumberFormatException.class, () ->
-                command.execute(incomes, expenseList));
-        assertEquals("Error: Expense entry not found.", exception.getMessage());
-    }
-
-    @Test
-    void updateExpense_updateMultipleFields() throws Exception {
-        UpdateExpenseCommand command = new UpdateExpenseCommand(
-                "update-expense 1 category/Transport amt/25.00 desc/Taxi d/15-03-2024");
-        command.execute(incomes, expenseList);
-
-        Expense updatedExpense = expenseList.get(0);
-        assertEquals("Transport", updatedExpense.getCategory());
-        assertEquals("Taxi", updatedExpense.getDescription());
-        assertEquals(25.00, updatedExpense.getAmount());
-        assertEquals("15-03-2024", updatedExpense.getDate());
-    }
-
-    @Test
-    void updateExpense_updateWithOnlyCategory() throws Exception {
-        UpdateExpenseCommand command = new UpdateExpenseCommand("update-expense 1 category/Leisure");
-        command.execute(incomes, expenseList);
-
-        Expense updatedExpense = expenseList.get(0);
-        assertEquals("Leisure", updatedExpense.getCategory());
-        assertEquals("Lunch", updatedExpense.getDescription());
-        assertEquals(10.50, updatedExpense.getAmount());
-        assertEquals("01-01-2024", updatedExpense.getDate());
+        assertEquals("Entertainment", updatedExpense.getCategory());
+        assertEquals("Movie", updatedExpense.getDescription());
+        assertEquals(15.00, updatedExpense.getAmount());
+        assertEquals("05-04-2024", updatedExpense.getDate());
     }
 }
-
-
-
