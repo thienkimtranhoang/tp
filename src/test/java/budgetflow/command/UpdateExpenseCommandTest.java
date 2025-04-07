4f720ef68bc537183e6bf243574a9d9ea3ed772a
dariusyawningwhiz <dariusleeql@gmail.com>
2025-04-07 20:07:29 +0800

    CI test

--- src/test/java/budgetflow/command/UpdateExpenseCommandTest.java
+++ src/test/java/budgetflow/command/UpdateExpenseCommandTest.java
@@ -16,7 +16,9 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 //@@author dariusyawningwhiz
 public class UpdateExpenseCommandTest {
